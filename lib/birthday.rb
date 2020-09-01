c4b4520438a9bcd49e4bcb7ab11d3153c9c6c16d
Rodney Smith II <r_smith3@hotmail.com>
2020-09-01 22:43:59 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -8,6 +8,6 @@ def happy_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
-end
+
   # add your code snippet here!
 end
